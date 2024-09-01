7c2c320496a94ce918c2402a1c160bd6a9a80d99
MuHua-123 <2960208585@qq.com>
2024-09-01 11:43:36 +0800

    1

--- Packages/com.muhua.tools/Runtime/UtilityTools/RayTool.cs
+++ Packages/com.muhua.tools/Runtime/UtilityTools/RayTool.cs
@@ -6,6 +6,28 @@ namespace MuHua {
     public static class RayTool {
         public static RaycastHit hitInfo;
         public static LayerMask DefaultLayerMask = ~(1 << 0) | 1 << 0;
+
+        /// <summary> 屏幕坐标转世界坐标 </summary>
+        public static bool GetScreenToWorldPosition(Vector3 screen, out Vector3 position) {
+            return GetScreenToWorldPosition(screen, out position, DefaultLayerMask);
+        }
+        /// <summary> 屏幕坐标转世界坐标 </summary>
+        public static bool GetScreenToWorldPosition(Camera camera, Vector3 screen, out Vector3 position) {
+            return GetScreenToWorldPosition(camera, screen, out position, DefaultLayerMask);
+        }
+        /// <summary> 屏幕坐标转世界坐标 </summary>
+        public static bool GetScreenToWorldPosition(Vector3 screen, out Vector3 position, LayerMask planeLayerMask) {
+            return GetScreenToWorldPosition(Camera.main, screen, out position, planeLayerMask);
+        }
+        /// <summary> 屏幕坐标转世界坐标 </summary>
+        public static bool GetScreenToWorldPosition(Camera camera, Vector3 screen, out Vector3 position, LayerMask planeLayerMask) {
+            Ray ray = camera.ScreenPointToRay(screen);
+            Physics.Raycast(ray, out hitInfo, 200, planeLayerMask);
+            position = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            if (hitInfo.transform != null) { position = hitInfo.point; }
+            return hitInfo.transform != null;
+        }
+
         public static bool GetMouseWorldSnappedPosition(out Vector3 position) {
             return GetMouseWorldSnappedPosition(out position, DefaultLayerMask);
         }
